189afca9a0cc7d967a7090fd7fdb3c07a9556ebb
valen10jr <valentinlanzillotta@hotmail.com>
2022-10-22 00:15:31 -0300

    Fin del proyecto

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 const express = require("express");
-
 const app = express();
+// const path = require('path');
 
-app.listen(3000, ()=> {
-console.log("Servidor escuchando en puerto", 3000)
-})
+app.use(express.static('public'));
+
+app.set('puerto',process.env.PORT || 3002)
+
+app.listen(app.get('puerto'), ()=>console.log(`Al fin estoy funcionando en el puerto ${app.get('puerto')}`));
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
